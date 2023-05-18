eb47510ed4782f3fa6ba3a769a8c15a4072808a0
claranceJ <77950647+claranceJ@users.noreply.github.com>
2023-05-18 08:43:08 +0000

    Added the json api

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Nairobi, Kenya",
+    "salary": "ksh 320,000"
+  },
+  {
+    "id": 2,
+    "title": "Frontend Engineer",
+    "location": "Nairobi, Kenya",
+    "salary": "ksh 216,000"
+  },
+  {
+    "id": 3,
+    "title": "Backend Engineer",
+    "location": "Nairobi, Kenya",
+    "salary": "ksh 369,000"
+  },
+  {
+    "id": 4,
+    "title": "CyberSecurity Analyst",
+    "location": "Nairobi, Kenya",
+    "salary": "ksh 199,000"
+  }
+]
+
+# to access the job items in json format: REST API, JSON API, API ENDPOINT
+@app.route("/api/jobs") #application programming interface
+def list_jobs():
+  return jsonify(JOBS)
+
+
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",
+                         jobs = JOBS,
+                        company_name = "KaicyLTD")
 
 
 if __name__ == "__main__":
