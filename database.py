e398cbc8ae495c02d6558b6ce2c2c94900ee403d
claranceJ <77950647+claranceJ@users.noreply.github.com>
2023-06-14 05:58:45 +0000

    converted row sequence object into dictionary and appended it

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://0kh237k411owp2ogpcot:pscale_pw_IaNqKxQI046JW1IDSMVycmEc1MnuI1LTBZSsvTasrwo@aws.connect.psdb.cloud/kaicyltdcareers?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://h3objrla19mgbxpvqfv0:pscale_pw_QbsUJwGVXwaW7AblwT8tLmtz0VjBZIFQMRQLlbvk0lR@aws.connect.psdb.cloud/kaicyltdcareers?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
@@ -9,4 +9,9 @@ engine = create_engine(db_connection_string,
 
 with engine.connect() as conn:
   result = conn.execute(text("select * from jobs"))
-  print(result.all())
+
+  result_dicts = []
+  for row in result.all():
+    result_dicts.append(dict(row._mapping))
+
+  print(result_dicts)
