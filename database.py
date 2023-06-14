69ff574e69aff9472c7ab2fb863117b9293cd4be
claranceJ <77950647+claranceJ@users.noreply.github.com>
2023-06-14 05:06:42 +0000

    enables connection to the cloud database, planetscale

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine(
-  "mysql+pymysql://0kh237k411owp2ogpcot:pscale_pw_IaNqKxQI046JW1IDSMVycmEc1MnuI1LTBZSsvTasrwo@aws.connect.psdb.cloud/kaicyltdcareers?charset=utf8mb4"
-)
+db_connection_string = "mysql+pymysql://0kh237k411owp2ogpcot:pscale_pw_IaNqKxQI046JW1IDSMVycmEc1MnuI1LTBZSsvTasrwo@aws.connect.psdb.cloud/kaicyltdcareers?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 with engine.connect() as conn:
-  result = conn.exe
+  result = conn.execute(text("select * from jobs"))
+  print(result.all())
